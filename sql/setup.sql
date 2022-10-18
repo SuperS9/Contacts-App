d441397788978375b129069d57e9450c17d7c96c
SuperS9 <103390623+SuperS9@users.noreply.github.com>
2022-10-19 00:26:18 +0200

    Create setup.sql

---
+++ sql/setup.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS contacts_app;
+
+CREATE DATABASE contacts_app;
+
+USE contacts_app;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+INSERT into users(name, email, password) VALUES("test", "test@test.com", "1234");
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    user_id INT NOT NULL,
+    phone_number VARCHAR(255),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
